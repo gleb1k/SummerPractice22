c0f58d6a1a58e829024f7352f34126ee23da6bfe
Gleb Gafeev <gleb.gafeev@mail.ru>
2022-07-02 14:10:34 +0300

    Вроде как исправил ошибки

--- app/src/main/java/com/example/summerpractice22/MainActivity.kt
+++ app/src/main/java/com/example/summerpractice22/MainActivity.kt
@@ -7,16 +7,9 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import java.lang.Exception
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
-
-    private var button: Button? = null
-    private var textView: TextView? = null
-    private var image: ImageView? = null
-    private var editTextName: EditText? = null
-    private var editTextNumberHeight: EditText? = null
-    private var editTextNumberWeight: EditText? = null
-    private var editTextNumberAge: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,32 +17,38 @@ class MainActivity : AppCompatActivity() {
 
         val button: Button = findViewById(R.id.button_calc)
         val textView: TextView = findViewById(R.id.textViewAnswer)
-        val image: ImageView = findViewById(R.id.mainImage)
+        //val image : Image = findViewById(R.id.mainImage)
         val editTextName: EditText = findViewById(R.id.editTextName)
         val editTextNumberHeight: EditText = findViewById(R.id.editTextNumberHeight)
         val editTextNumberWeight: EditText = findViewById(R.id.editTextNumberWeight)
         val editTextNumberAge: EditText = findViewById(R.id.editTextNumberAge)
 
 
-        button?.setOnClickListener {
-            val name: String = editTextName?.getText().toString()
-            val height: Int = Integer.parseInt(editTextNumberHeight?.getText().toString())
-            val weight: Int = Integer.parseInt(editTextNumberWeight?.getText().toString())
-            val age: Int = Integer.parseInt(editTextNumberAge?.getText().toString())
-
-            //Как сделать красивее не придумал
-            if ((name.length > 50) || (height <= 0 || height >= 250)
-                || (weight <= 0 || weight >= 250) || (age <= 0 || age >= 150)
-            ) {
-                textView.setText("Invalid values :с")
-            } else {
-                val result: Long = Math.round(88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age))
-                textView.setText("$name Your callories is:  $result")
+        button.setOnClickListener {
+            if (editTextNumberAge.text.isEmpty() || editTextName.text.isEmpty() ||
+                editTextNumberHeight.text.isEmpty() ||editTextNumberWeight.text.isEmpty()){
+                textView.text = "Fields are empty"
+                return@setOnClickListener
             }
 
-            //BMR = 88,36 + (13,4 × вес в кг) + (4,8 × рост в см) – (5,7 × возраст в годах).
+            val name: String = editTextName.text.toString()
+            val height: Int = (editTextNumberHeight.text.toString()).toInt()
+            val weight: Float = editTextNumberWeight.text.toString().toFloat()
+            val age: Int = (editTextNumberAge.text.toString()).toInt()
+
+            //Как сделать красивее не придумал
+            if ((name.length > 50) || (height !in 0..250)
+                || (weight <= 0 || weight >= 250) || (age !in 0..150)
+            ) {
+                textView.text = "Invalid values :с"
+            } else {
+                val result: Int =
+                    (88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age) + name.length).roundToInt()
+                textView.text = "Answer:  $result"
+
+                //coolFormula = 88,36 + (13,4 × вес в кг) + (4,8 × рост в см) – (5,7 × возраст в годах) + длина имени.
+            }
 
         }
-
     }
 }
